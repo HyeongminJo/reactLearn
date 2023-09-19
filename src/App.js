0f7679a8e3831ca781ec4a233c0bee181109d0e3
rainbow <120556318+seoh0426@users.noreply.github.com>
2023-09-20 01:09:53 +0900

    react
    230920

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}디자이너 Hwara Jo
+        </p>
+        <a
+          className="App-link"
+          // href="https://reactjs.org"
+          href="https://www.instagram.com/hwararararara/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {/* Learn React */} @hwararararara
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
